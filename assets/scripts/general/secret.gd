13f89c6183126ae07323e0d0ee79a2cdfc5327c0
MasterCheap9447 <shreyaemsahu@gmail.com>
2025-07-05 22:34:49 +0530

    FWAEH

--- assets/scripts/general/secret.gd
+++ assets/scripts/general/secret.gd
@@ -1,23 +1,19 @@
 extends Node3D
 
 
-@onready var check: RayCast3D = $check
-@onready var player: CharacterBody3D = $"../player"
-@onready var explosion: GPUParticles3D = $explosion
-@onready var model: MeshInstance3D = $model
+@onready var player_area: Area3D = $"player area"
+@onready var cool_s: MeshInstance3D = $"cool S"
+
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
-	check.rotation.x += 1
-	check.rotation.z += 1
-	check.rotation.y += 1
-	if check.is_colliding():
-		var target = check.get_collider()
-		if target.is_in_group("Player"):
-			model.hide()
-			explosion.emitting = true
-			await  get_tree().create_timer(0.5).timeout
-			queue_free()
+	
+	cool_s.rotation.y += deg_to_rad(0.1)
+	
+	if player_area.has_overlapping_bodies():
+		for p in player_area.get_overlapping_bodies():
+			if player_area.is_in_group("Player"):
+				queue_free()
 	pass
